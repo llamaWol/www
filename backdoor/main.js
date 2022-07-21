e372dc51822db1e0e23260a5929c0c293c9f4bcb
llamaWol <llamawxl@gmail.com>
2022-07-21 22:03:20 +0200

    Init

---
+++ backdoor/main.js
@@ -0,0 +1,14 @@
+console.log("Joost");
+
+//    Colour lemon(s) on click    //
+let lemons = document.querySelectorAll(".fa-lemon");
+
+console.log(`${lemons.length} lemon(s)`)
+
+for (let i = 0; i < lemons.length; i++) {
+	let lemon = lemons[i];
+
+	lemon.addEventListener('click', () => {
+		lemon.classList.add('lemon');
+	});
+}
